5911b8cee17af83aa74a0e8f0f04cef34c7e67f9
Rinyunfui <19cu0236@jec.ac.jp>
2020-11-22 22:04:45 +0900

    [更新] ミッションの達成数を表示、ページ数に表示。

--- Game/Source/hukuokaGame/PlayerCharacter.h
+++ Game/Source/hukuokaGame/PlayerCharacter.h
@@ -358,15 +358,14 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "VR_Phone|Mission")
 		int finished_MsiionID;						// テスト用　　　完成したミッションID　（表示したくないIDを入れる）
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerStatus")
+		TArray<int> missionStatus ;					// ミッションの完了状態を管理するarray
 
 	UFUNCTION(BlueprintCallable, Category = "VR_Phone|Mission")
 		void UpdateTheMission(bool _isDelete, int _missionID);	// _missionIDというミッションのフラグと表示を処理する
 
 	UFUNCTION(BlueprintCallable, Category = "VR_Phone|Mission")
 		int GetTheWideStringsByteLength(FString _inString, FText _inText);		// 
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR_Phone|Mission")
-		FString testingStr;		// 今表示するミッション　（今は使えていない）
 
 	bool isFound;		// 敵の攻撃範囲内に入ったか(作成者：尾崎)
 
